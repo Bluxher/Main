49c701acfa16fe0d8ac416a42e6dc6859f7f146e
Mirko <jaroslavad2004@gmail.com>
2025-01-06 12:54:54 -0500

    Day 2

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const app = Vue.createApp({
     data() {
         return{
-            showBooks: true,
-            title: "The Final Empire",
-            author: 'Brandon Sanderon',
-            age: 45
+            showBooks: true,          
+            // title: "The Final Empire",
+            // author: 'Brandon Sanderon',
+            // age: 45,
+            // x: 0,
+            // y: 0,
+
+            books:[
+                {title: 'name of the wind', author: 'patrick rothfus', img: 'Assets/1.jpg', isFav: true},
+                {title: 'the way of kings', author: 'brandon sanderson', img: 'Assets/2.jpg', isFav: false},
+                {title: 'the final empire', author: 'brandon sanderson', img: 'Assets/3.jpg', isFav: true}
+            ],
+
+            url: 'https://www.youtube.com/watch?v=CYPZBK8zUik&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=3'
         };
     },
     methods: {
@@ -13,6 +23,25 @@ const app = Vue.createApp({
         // }
         toggleShowBooks() {
             this.showBooks = !this.showBooks
+        },
+        // handleEvent(e, data) {
+        //     console.log(e, e.type);
+        //     if(data) {
+        //         console.log(data);
+        //     }
+        // },
+        // handleMouseMove(e){
+        //     this.x= e.offsetX
+        //     this.y= e.offsetY
+        // }
+        toggleFavStatus(e) {         
+            e.isFav= !e.isFav
+            console.log(e)
+        }
+    },
+    computed: {
+        filteredBooks(){
+            return this.books.filter((book) => book.isFav)
         }
     }
 });
