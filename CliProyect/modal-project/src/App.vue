b7c7d8a7f77bdbfe6b751eae662473282fa85d0d
Mirko <jaroslavad2004@gmail.com>
2025-01-08 12:08:42 -0500

    Completed Modal Project

--- CliProyect/modal-project/src/App.vue
+++ CliProyect/modal-project/src/App.vue
@@ -1,29 +1,64 @@
 <template>
   <h1>{{ title }}</h1>
-  <input type="text" ref="name">
-  <button @click="handleClick">click me</button>
+  <p>Welcome.....</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">Sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      
+      <h1>Ninja Giveaway!</h1>
+      <p>Grab your ninja swag for half price!</p>
+      
+    </Modal>
+  </teleport>
+  
+  <telepor v-if="showModalTwo">
+    <Modal theme="asdsadasd" @close="toggleModalTwo">
+      <h1>WAAAAAAAAAAAAAA</h1>
+    </Modal>
+  </telepor>
+  <button @click.alt="toggleModal">Open Modal(alt)</button>
+  <button @click="toggleModalTwo">Open Modal Two</button>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">click me</button> -->
 </template>
 
 <script>
+import Modal from './components/Modal.vue';
 export default {
   name: 'App',
+  components: { Modal },
   data() {
     return {
-      title: 'My First Vue App'
+      title: 'My First Vue App',
+      header: 'Sign up for the Giveaway',
+      text: 'jsjsjsjsjsjsjsjsjsjsjsjsjsj',
+      showModal: false,
+      showModalTwo: false
     }
   },
   methods: {
-    handleClick() {
-      console.log(this.$refs.name)
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
+  // methods: {
+  //   handleClick() {
+  //     console.log(this.$refs.name)
+  //     this.$refs.name.classList.add('active')
+  //     this.$refs.name.focus()
+  //   }
+  // }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
