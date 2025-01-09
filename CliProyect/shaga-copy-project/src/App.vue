5316265edddd75cdb4abd4d12bf62c6079afe03d
Mirko <jaroslavad2004@gmail.com>
2025-01-09 12:58:35 -0500

    New project copy shaga website

---
+++ CliProyect/shaga-copy-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <Navbar/>
+  <router-view/>
+</template>
+
+<script>
+import Navbar from './components/NavBar/Navbar';
+export default {
+  components: { Navbar}
+}
+</script>
+
+<style>
+body {
+  background: rgb(59, 58, 58);
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #e9e9e9;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ CliProyect/shaga-copy-project/src/components/NavBar/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="Navbar">
+    <ul>
+      <li>wa</li>
+      <li>wa</li>
+      <li>wa</li>
+      <li>wa</li>
+      <li>wa</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.Navbar {
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  background-color: rgba(255, 255, 255, 0.0003);
+  backdrop-filter: blur(30px);  
+  justify-content: space-between;
+  align-items: center;
+  transition: 0,5s;
+}
+</style>
